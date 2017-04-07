5aff2f3b620d5281024b69a0031ce2e89f681df0
zxj5470 <zxj5470@foxmail.com>
2017-04-07 13:28:23 +0800

    update for characters problems with two 'major pollutant'

---
+++ cn/wjdghd/replaceAllChar.py
@@ -0,0 +1,18 @@
+# coding : utf-8
+import os
+
+f_list = os.listdir('../')
+for i in f_list:
+    if os.path.splitext(i)[1] == '.csv':
+        print(str(i))
+        try:
+            f = open('../' + str(i), 'rb')
+            f2 = open('../New'+str(i), 'wb')
+            all_the_text = f.read()
+            strall = str(all_the_text, encoding='gbk')
+            strall = strall.replace("\r\n", "", 100000000).replace('\r\r', '\r', 100000000).replace('  ', '',
+                                                                                                    100000000)
+            f2.write(bytes(strall, encoding='gbk'))
+        finally:
+            f.close()
+            f2.close()
