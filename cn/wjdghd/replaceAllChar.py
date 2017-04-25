dc4afeb6f71a008ed798f21404d3a194d7eae8d1
zxj5470 <zxj5470@foxmail.com>
2017-04-25 13:00:53 +0800

    git push replaceAllChar

--- cn/wjdghd/replaceAllChar.py
+++ cn/wjdghd/replaceAllChar.py
@@ -2,12 +2,13 @@
 import os
 
 f_list = os.listdir('../')
+os.mkdir('New')
 for i in f_list:
     if os.path.splitext(i)[1] == '.csv':
         print(str(i))
         try:
             f = open('../' + str(i), 'rb')
-            f2 = open('../New'+str(i), 'wb')
+            f2 = open('../New/'+str(i), 'wb')
             all_the_text = f.read()
             strall = str(all_the_text, encoding='gbk')
             strall = strall.replace("\n", "", 100000000).replace("\r\n", "", 100000000).replace('  ', '',100000000).replace('\r\r', '\r', 100000000)
